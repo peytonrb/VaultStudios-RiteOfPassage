a1b667e9402d288ada3ec3d02e69ac37bc71c4ea
Peyton Bischof <peyton.bischof@hotmail.com>
2023-03-06 20:26:40 -0500

    PlayerController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSens = 3.0f;
+    private float rotationY;
+    private float rotationX;
+    public Transform target;
+    public float targetDistance = 3.0f;
+    private Vector3 currentRotation;
+    private Vector3 velocity = Vector3.zero;
+    private float time = 0.2f;
+    private Vector2 rotationXMinMax = new Vector2(-40, 40);
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens;
+
+        rotationY += mouseX;
+        rotationX += mouseY;
+
+        // clamp rotations
+        rotationX = Mathf.Clamp(rotationX, rotationXMinMax.x, rotationXMinMax.y);
+        Vector3 nextRotation = new Vector3(rotationX, rotationY);
+        currentRotation = Vector3.SmoothDamp(currentRotation, nextRotation, ref velocity, time);
+        transform.localEulerAngles = currentRotation;
+        transform.position = target.position - transform.forward * targetDistance;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed;
+    public float sensitivity;
+    public new Camera camera;
+    private Vector3 velocity;
+    private bool isGrounded;
+    public float jumpHeight;
+    private float gravity = -9.81f;
+
+    private void Start() 
+    {
+        controller = GetComponent<CharacterController>();    
+    }
+
+    private void Update() 
+    {
+        Movement();    
+    }
+
+    void Movement() 
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = 0f;
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementInput = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0) * new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 movementDirection = movementInput.normalized;
+
+        controller.Move(movementDirection * speed * Time.deltaTime);
+
+        // player faces movement direction
+        if (movementDirection != Vector3.zero) 
+        {
+            Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, sensitivity * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
