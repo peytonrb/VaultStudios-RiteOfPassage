1e6642e136efbafcb9508ac8365b87ecf8d1fd2b
Peyton Bischof <peyton.bischof@hotmail.com>
2023-03-20 14:16:30 -0400

    Grappling Hook

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -8,12 +8,11 @@ public class GrapplingHook : MonoBehaviour
     public GameObject hook;
     public GameObject hookHolder;
     public LayerMask isGrappable;
-    private Transform debugTransform;
 
     [Header("Variables")]
     public float hookTravelSpeed;
     public float playerTravelSpeed;
-    public static bool hooked;
+    public bool hooked;
     public static bool fired;
     public float maxDistance;
     private float currentDistance;
@@ -25,20 +24,26 @@ public class GrapplingHook : MonoBehaviour
             fired = true;
         }
 
-        if (fired) {
-            // hook.transform.Translate(Vector3.forward * Time.deltaTime * hookTravelSpeed);
-            Vector2 crosshair = new Vector2(Screen.width / 2f, Screen.height / 2f);
-            Ray ray = Camera.main.ScreenPointToRay(crosshair);
-
-            if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, isGrappable)) {
-                debugTransform.position = raycastHit.point;
-            }
-
+        if (fired && !hooked) {
+            hook.transform.Translate(Vector3.forward * Time.deltaTime * hookTravelSpeed);
             currentDistance = Vector3.Distance(transform.position, hook.transform.position);
 
             if (currentDistance >= maxDistance) {
                 returnHook();
             }
+        }
+
+        if (hooked) {
+            transform.position = Vector3.MoveTowards(transform.position, hook.transform.position, Time.deltaTime * playerTravelSpeed);
+            float distanceFromHook = Vector3.Distance(transform.position, hook.transform.position);
+            this.GetComponent<Rigidbody>().useGravity = false;
+
+            if (distanceFromHook < 1)
+            {
+                returnHook();
+            }
+        } else {
+            this.GetComponent<Rigidbody>().useGravity = true;
         }
     }
 
--- Assets/Scripts/HookDetector.cs
+++ Assets/Scripts/HookDetector.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class HookDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider collision)
     {
-        
+        if (collision.tag == "isGrappable")
+        {
+            player.GetComponent<GrapplingHook>().hooked = true;
+        }
     }
 }
