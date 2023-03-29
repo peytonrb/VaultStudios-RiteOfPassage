80faad2b9574ec4683a9726041701c2bff1070ed
Peyton Bischof <peyton.bischof@hotmail.com>
2023-03-29 14:27:43 -0400

    Shader pt 3

--- Assets/Shaders/ToonShading.hlsl
+++ Assets/Shaders/ToonShading.hlsl
@@ -1,5 +1,5 @@
 void ToonShading_float(in float3 Normal, in float ToonRampSmoothness, in float3 ClipSpacePos, in float3 WorldPos, in float4 ToonRampTinting,
-                    in float ToonRampOffset, out float3 ToonRampOutput, out float3 Direction)
+                in float ToonRampOffset, out float3 ToonRampOutput, out float3 Direction)
 {
  
     // set the shader graph node previews
