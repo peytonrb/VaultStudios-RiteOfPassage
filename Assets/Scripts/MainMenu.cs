385eede9ebcecbb3d8da58fac0b82a834597f9c6
Noah-OConnor <122512750+Noah-OConnor@users.noreply.github.com>
2023-04-01 21:57:09 -0400

    mainMenuTime:0

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject howToPlay;
+
+    public void Awake()
+    {
+        mainMenu.SetActive(true);
+        howToPlay.SetActive(false);
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Cave");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void HowToPlayButton()
+    {
+        mainMenu.SetActive(false);
+        howToPlay.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        mainMenu.SetActive(true);
+        howToPlay.SetActive(false);
+    }
+}
