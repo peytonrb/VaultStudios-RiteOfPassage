9bf916db3cafae05266c06159b3f1b0b4b4dc181
Noah-OConnor <122512750+Noah-OConnor@users.noreply.github.com>
2023-04-02 01:43:47 -0400

    AddedAudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,86 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (Instance != null) 
+        {
+            DestroyImmediate(gameObject);
+        } 
+        else 
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            
+        }
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+            //s.source.pitch = s.pitch;
+        }
+    }
+    private void OnDestroy()
+    {
+        
+        if (Instance == this) 
+        {
+            Instance = null;
+        }
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("No sound found");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("No sound found");
+            return;
+        }
+        s.source.Stop();
+    }
+
+    public void Pause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("No sound found");
+            return;
+        }
+        s.source.Pause();
+    }
+
+    public void UnPause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("No sound found");
+            return;
+        }
+        s.source.UnPause();
+    }
+}
