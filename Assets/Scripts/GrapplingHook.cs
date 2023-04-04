0638da2a9293ba4a58a222c1a31951614c5680cb
Peyton Bischof <peyton.bischof@hotmail.com>
2023-04-04 11:42:59 -0400

    FIXED GRAPPLE!! :D

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -18,23 +18,27 @@ public class GrapplingHook : MonoBehaviour
     public static bool fired;
     public GameObject hookedObject;
     public float maxDistance;
+    public float hookProximity; // how close to hook will player release grapple and drop to ground
     private float currentDistance;
     private bool grounded;
     private Animator animator;
 
-    void Update() {
+    void Update()
+    {
         animator = GetComponent<Animator>();
         CharacterController charController = this.GetComponent<CharacterController>();
         Rigidbody rigidbody = this.GetComponent<Rigidbody>();
         player = this.GetComponent<PlayerController>();
 
         // firing the hook
-        if (Input.GetButtonDown("Grapple") && !fired) {
+        if (Input.GetButtonDown("Grapple") && !fired)
+        {
             fired = true;
         }
 
         // creates visual line (rope)
-        if (fired) {
+        if (fired)
+        {
             LineRenderer rope = hook.GetComponent<LineRenderer>();
             rope.positionCount = 2;
             rope.SetPosition(0, hookHolder.transform.position);
@@ -42,52 +46,55 @@ public class GrapplingHook : MonoBehaviour
         }
 
         // if fired but not hooked, hook continues to travel and/or returns to player
-        if (fired && !hooked) {
+        if (fired && !hooked)
+        {
             float x = Screen.width / 2;
             float y = Screen.height / 2;
             RaycastHit hit;
             Ray ray = cam.ScreenPointToRay(new Vector3(x, y, 0));
 
-            if (Physics.Raycast(ray, out hit, maxDistance, 9)) {
-                if (hit.collider.tag == "isGrappable") {
-                    Debug.Log(hit.collider.gameObject); // collides with right object :/
+            if (Physics.Raycast(ray, out hit, maxDistance, 9))
+            {
+                if (hit.collider.tag == "isGrappable")
+                {
                     transform.LookAt(hit.point); // in case player is backwards when fire
                     hook.transform.LookAt(hit.point);
-                    hook.transform.Translate(ray.direction * hookTravelSpeed * Time.deltaTime);
+                    hook.transform.position = Vector3.MoveTowards(hook.transform.position, hit.point, maxDistance);
+                    // hook.transform.Translate(ray.direction * hookTravelSpeed * Time.deltaTime);
                     currentDistance = Vector3.Distance(transform.position, hook.transform.position);
 
-                    if (currentDistance >= maxDistance) {
+                    if (currentDistance >= maxDistance)
+                    {
                         returnHook();
                     }
                 }
-            } else {
+            }
+            else
+            {
                 returnHook();
             }
         }
 
         // performs complete grapple
-        if (hooked && fired) {
+        if (hooked && fired)
+        {
             hook.transform.parent = hookedObject.transform;
             charController.enabled = false;
-            transform.position = Vector3.MoveTowards(transform.position, hook.transform.position, 
+            transform.position = Vector3.MoveTowards(transform.position, hook.transform.position,
                                                     Time.deltaTime * playerTravelSpeed);
             charController.enabled = true;
             float distanceFromHook = Vector3.Distance(transform.position, hook.transform.position);
             rigidbody.useGravity = false;
             animator.SetBool("IsGrappling", true);
 
-            if (distanceFromHook < 1)
+            if (distanceFromHook < hookProximity)
             {
-                // player will climb up wall if close enough to top
-                if (!grounded) {
-                    this.transform.Translate(Vector3.forward * Time.deltaTime * 13f);
-                    this.transform.Translate(Vector3.up * Time.deltaTime * 17f);
-                }
-
-                StartCoroutine("Climb");
-                // returnHook();
+                // StartCoroutine("Climb");
+                returnHook();
             }
-        } else {
+        }
+        else
+        {
             hook.transform.parent = hookHolder.transform;
             rigidbody.useGravity = true;
             charController.enabled = true;
@@ -96,13 +103,15 @@ public class GrapplingHook : MonoBehaviour
     }
 
     // initiates so player does not glitch into corners if the hook grabs the edge of a block
-    private IEnumerator Climb() {
+    private IEnumerator Climb()
+    {
         yield return new WaitForSeconds(0.1f);
         returnHook();
     }
 
     // returns hook to original position
-    private void returnHook() {
+    private void returnHook()
+    {
         hook.transform.rotation = hookHolder.transform.rotation;
         hook.transform.position = hookHolder.transform.position;
         fired = false;
@@ -112,14 +121,18 @@ public class GrapplingHook : MonoBehaviour
     }
 
     // helper method, performs ground check 
-    private void isGrounded() {
+    private void isGrounded()
+    {
         RaycastHit hit;
         float distance = 1f;
         Vector3 direction = new Vector3(0, -1);
 
-        if (Physics.Raycast(transform.position, direction, out hit, distance)) {
+        if (Physics.Raycast(transform.position, direction, out hit, distance))
+        {
             grounded = true;
-        } else {
+        }
+        else
+        {
             grounded = false;
         }
     }
