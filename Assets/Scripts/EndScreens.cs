9804c4cee21b01e180652e24902749ad3ae034d4
Noah-OConnor <122512750+Noah-OConnor@users.noreply.github.com>
2023-04-30 16:59:12 -0400

    FinalSequence

--- Assets/Scripts/EndScreens.cs
+++ Assets/Scripts/EndScreens.cs
@@ -6,30 +6,114 @@ using UnityEngine.UI;
 
 public class EndScreens : MonoBehaviour
 {
+    //Menus
     public GameObject winText;
     public GameObject loseText;
     public Button mainMenuButton;
+    public GameObject credits;
+    public GameObject exitButton;
+
+    //Credits
+    public float startY;
+    public float endY;
+    public float creditTime;
+    public Image space;
+
+    //Cutscene
+    public Sprite[] frames;
+    public float timeBetween;
+    public Image cutscene;
+    
+
 
     private void Start()
     {
         mainMenuButton.Select();
-    } 
-    
-    private void Update()
-    {
         if(GameManager.Instance.win)
         {
-            winText.SetActive(true);
-            loseText.SetActive(false);
+            StartCoroutine(winSequence(creditTime, new Vector3(960, startY, 0), new Vector3(960, endY, 0)));
         }
         else
         {
-            loseText.SetActive(true);
-            winText.SetActive(false);
+            //loseText.SetActive(true);
+            //winText.SetActive(false);
         }
 
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+    } 
+    
+    private void Update()
+    {
+    }
+
+    private IEnumerator winSequence(float time, Vector3 startPos, Vector3 endPos)
+    {
+        //exitButton.SetActive(false);
+        //mainMenuButton.gameObject.SetActive(false);
+        winText.SetActive(true);
+        loseText.SetActive(false);
+        credits.SetActive(true);
+        cutscene.gameObject.SetActive(true);
+
+        Image creditImage = credits.GetComponent<Image>();
+        var tempColor = creditImage.color;
+        tempColor.a = 0;
+
+        space.color = tempColor;
+        creditImage.color = tempColor;
+
+
+        for (int i = 0; i < frames.Length; i++)
+        {
+            cutscene.sprite = frames[i];
+
+            yield return new WaitForSeconds(timeBetween);
+        }
+
+        float elapsedTime = 0;
+        while (elapsedTime < 2)
+        {
+            tempColor.a += elapsedTime * Time.deltaTime;
+            creditImage.color = tempColor;
+            space.color = tempColor;
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        elapsedTime = 0;
+        while (elapsedTime < time)
+        {
+            credits.transform.position = Vector3.Lerp(startPos, endPos, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+        cutscene.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+        
+        /*elapsedTime = 0;
+        while (elapsedTime < time)
+        {
+            credits.transform.position = Vector3.Lerp(endPos, new Vector3(endPos.x, endPos.y + endY - startY, endPos.z), (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }*/
+
+        elapsedTime = 0;
+        while (elapsedTime < 2)
+        {
+            tempColor.a -= elapsedTime * Time.deltaTime;
+            creditImage.color = tempColor;
+            space.color = tempColor;
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        credits.SetActive(false);
     }
 
     public void MainMenuButton()
